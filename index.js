e535da8774aa29c93177c98ba93ab813052252ba
Rohan Darandale <113358012+rohandarandale1144@users.noreply.github.com>
2023-12-26 13:29:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontens = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontens) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
